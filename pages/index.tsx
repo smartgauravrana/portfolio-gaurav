bef7edd715db47f06b488492a54fdf19415fadb3
Gaurav Rana <smartgauravrana@gmail.com>
2022-10-06 08:50:00 +0530

    Header and hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="bg-[#242424] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Gaurav's Portfolio</title>
       </Head>
-      <h1 className="text-red-500">Hey let;s build</h1>
+      {/* Header */}
+      <Header />
+
+      {/* Hero */}
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
     </div>
   );
 };
